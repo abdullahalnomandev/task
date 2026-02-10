e6a30d73b823847618a4269379726154f55183f8
abdullahalnomandev <abdullahalnoman1512@gmail.com>
2026-02-10 09:38:01 +0600

    update

--- src/helpers/firebaseConfig.ts
+++ src/helpers/firebaseConfig.ts
@@ -3,17 +3,6 @@ import admin from 'firebase-admin';
 import path from 'path';
 import fs from 'fs';
 
-const serviceAccountPath = path.join(process.cwd(), 'fcmServiceAccountKey.json');
 
-if (!fs.existsSync(serviceAccountPath)) {
-  throw new Error(
-    `Service account key file not found at ${serviceAccountPath}. Please ensure the file exists.`
-  );
-}
-
-
-admin.initializeApp({
-  credential: admin.credential.cert(JSON.parse(fs.readFileSync(serviceAccountPath, 'utf8'))),
-});
 
 export default admin;
